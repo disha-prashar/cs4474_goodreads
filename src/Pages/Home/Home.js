6d68578e3cfc73d797506d9eb56151be33cfc4c6
Valerie Lozano <vlozanor@uwo.ca>
2024-03-29 19:01:54 -0400

    adding new components for HP

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Home.css';
 import ReadingChallengeSection from '../../Components/HomepageComponents/ReadingChallenge/ReadingChallenge';
+import TrendingThisWeek from '../../Components/HomepageComponents/TrendingThisWeek/TrendingThisWeek';
+import FavouriteReadingLists from '../../Components/HomepageComponents/FavouriteReadingLists/FavouriteReadingLists';
 
 function Home() {
   return (
     <div className='mainPage'>
         <ReadingChallengeSection />
+        <TrendingThisWeek />
+        <FavouriteReadingLists />
     </div>
   );
 }
