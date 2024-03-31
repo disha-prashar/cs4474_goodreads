4dcc00fa8cd3208293df717af36ab7929fef30e9
Disha Prashar <dishaprashar@gmail.com>
2024-03-30 20:04:24 -0400

    added component on home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 import ReadingChallengeSection from '../../Components/HomepageComponents/ReadingChallenge/ReadingChallenge';
 import TrendingThisWeek from '../../Components/HomepageComponents/TrendingThisWeek/TrendingThisWeek';
+import BookCategoriesTabs from '../../Components/HomepageComponents/BookCategories/BookCategories';
 import FavouriteReadingLists from '../../Components/HomepageComponents/FavouriteReadingLists/FavouriteReadingLists';
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
     <div className='mainPage'>
         <ReadingChallengeSection />
         <TrendingThisWeek />
+        <BookCategoriesTabs />
         <FavouriteReadingLists />
     </div>
   );
