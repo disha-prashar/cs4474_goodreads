69dc953233536540bdc5f848ba25b13a9ee66114
Eiman-MZ <162847420+Eiman-MZ@users.noreply.github.com>
2024-04-01 12:45:49 -0400

    Update MyBooksSidebar.js

--- src/Components/MyBooksSidebar/MyBooksSidebar.js
+++ src/Components/MyBooksSidebar/MyBooksSidebar.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import '../../Pages/MyBooks/MyBooks.css';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './MyBooksSidebar.css';
 
 function MyBooksSidebar() {
     return (
